833314d0c51bb0eed43c0e9be276a48a49479a84
astaph0r <ankitbhanja99@gmail.com>
2023-10-18 05:22:15 +0530

    deploy fix

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -4,7 +4,7 @@ const { v4: uuidV4 } = require("uuid");
 const socketServer = (server) => {
 	const io = new Server(server, {
 		cors: {
-			origin: "http://localhost:5173",
+			origin: process.env.CORS,
 		},
 	});
 
